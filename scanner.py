dd5d90e891c3181fa31d320cb8945851bd6b99c5
pankajjarial360 <87800233+pankajjarial360@users.noreply.github.com>
2023-05-07 11:30:43 +0530

    changed the port number from (50,85) to (1,10000)

--- scanner.py
+++ scanner.py
@@ -21,7 +21,7 @@ print("Time started: "+str(datetime.now()))
 print("-" * 50)
 
 try:
-	for port in range (50,85):
+	for port in range (1,10000):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket.setdefaulttimeout(1)
 		result = s.connect_ex((target,port))	#returns an error indicator
