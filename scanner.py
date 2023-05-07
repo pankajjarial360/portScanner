0ab79ab8e4e3fb1b58dc07d31c2f43bb1e4b5c2c
pankajjarial360 <87800233+pankajjarial360@users.noreply.github.com>
2023-05-07 11:33:04 +0530

    added ssome space in between port and no.

--- scanner.py
+++ scanner.py
@@ -25,9 +25,9 @@ try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket.setdefaulttimeout(1)
 		result = s.connect_ex((target,port))	#returns an error indicator
-		print("checking port{}".format(port))
+		print("checking port {}".format(port))
 		if result ==0:
-			print ("Port {} is open".format(port))
+			print ("Port {} is open ".format(port))
 			s.close()
 
 except KeyboardInterrupt:
